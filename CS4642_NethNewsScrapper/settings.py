9675ba66734fca7d96558fb0d3a3f78a9375a08a
sudeepa14 <sudeepa.14@cse.mrt.ac.lk>
2018-06-20 05:50:19 +0100

    corpus added

--- CS4642_NethNewsScrapper/settings.py
+++ CS4642_NethNewsScrapper/settings.py
@@ -67,6 +67,7 @@ ROBOTSTXT_OBEY = True
 #ITEM_PIPELINES = {
 #    'CS4642_NethNewsScrapper.pipelines.Cs4642NethnewsscrapperPipeline': 300,
 #}
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
--- CS4642_NethNewsScrapper/spiders/NewsSpider.py
+++ CS4642_NethNewsScrapper/spiders/NewsSpider.py
@@ -12,10 +12,15 @@ class NewsSpider(scrapy.Spider):
     def parse(self, response):
         for news in response.css('div.breaking_news'):
             yield {
-                'title':news.xpath('//h3[@class="entry-title"]/a/@title').extract_first(),
-                'date': news.xpath('//div[@class="publish_date"]/text()').extract_first(),
-                'details': news.xpath('//div[@class="top_breaking_news_discription"]/text()').extract_first(),
-                'link': news.xpath('//h3[@class="entry-title"]/a/@herf').extract_first()
+                'title':news.css('h3 a::text').extract_first(),
+                'date': news.css('div.publish_date::text').extract_first(),
+                'details': news.css('div.top_breaking_news_discription::text').extract_first(),
+                'link': news.css('h3 a::attr(href)').extract_first().strip()
+                
+                # 'title':news.xpath('//h3[@class="entry-title"]/a/@title').extract_first(),
+                # 'date': news.xpath('//div[@class="publish_date"]/text()').extract_first(),
+                # 'details': news.xpath('//div[@class="top_breaking_news_discription"]/text()').extract_first(),
+                # 'link': news.xpath('//h3[@class="entry-title"]/a/@href').extract_first()
             }
 
         next_page = response.xpath('//a[@rel="next"]/@href').extract_first()
